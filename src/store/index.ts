9b9e511f003a0b9efbe000acaf4ca518762b4408
mengjie.zhang <mengjie.zhang@easystack.cn>
2023-04-02 14:26:46 +0800

    添加状态管理打印日志

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,23 @@
 import { configureStore, Action } from '@reduxjs/toolkit';
 import { createEpicMiddleware } from 'redux-observable';
+import { createLogger } from 'redux-logger'
 import reducers from './reducers';
 import { rootEpic$ } from './epics';
 import { StoreState } from './types';
 
 const epicMiddleware = createEpicMiddleware<Action, Action, StoreState>();
+const isDev = process.env.NODE_ENV === 'dev'
+const loggerMiddleware = createLogger({ collapsed: true });
+
+const middleware: any = [epicMiddleware]
+
+if(isDev){
+  middleware.push(loggerMiddleware)
+}
 
 export const store = configureStore({
   reducer: reducers,
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(epicMiddleware);
-  },
+  middleware: middleware
 });
 
 epicMiddleware.run(rootEpic$);
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions: {
+      // external: ['redux-logger'], // 将 redux-logger 作为外部模块
+      // output: {
+      //   // ...其他输出选项
+      //   globals: {
+      //     // 配置 redux-logger 全局变量
+      //     'redux-logger': 'ReduxLogger'
+      //   }
+      // }
+    }
+  }
 })
